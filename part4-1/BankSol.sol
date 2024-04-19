36f589ab38c0cf9668d6d0505ed37dcec5fa3e12
ace <>
2024-04-19 11:20:45 +0800

    fix gas

--- part4-1/BankSol.sol
+++ part4-1/BankSol.sol
@@ -8,11 +8,8 @@ contract Bank {
     address public owner;
     //定义每个账户
     mapping(address => uint) public balances;
-    //定义总共的钱
-    uint public totalBalance;
-
+    //定义top3存款人
     address[3] public top3Depositors;
-    uint[3] public top3Amounts;
 
     constructor() {
         owner = msg.sender;
@@ -23,65 +20,47 @@ contract Bank {
         _;
     }
 
-    // 1.存款方法，接受一个地址和一个金额作为参数
+    // 1.存款方法
     function deposit() external payable {
         balances[msg.sender] += msg.value;
-        totalBalance += msg.value;
-
-        //更新前三名
-        updateTopThree(msg.sender, msg.value);
+        if(balances[msg.sender] > balances[top3Depositors[2]]){
+            if (balances[msg.sender] >  balances[top3Depositors[0]]) {
+                top3Depositors[2] = top3Depositors[1];
+                top3Depositors[1] = top3Depositors[0];
+                top3Depositors[0] = msg.sender;
+            } else if (balances[msg.sender] > balances[top3Depositors[1]]){
+                top3Depositors[2] = top3Depositors[1];
+                top3Depositors[1] = msg.sender;
+            } else {
+                top3Depositors[2] = msg.sender;
+            }
+        }
     }
 
     // 2. 管理员取所有钱
     function withdraw() public onlyOwner {
         uint ubalance = address(this).balance;
         require(ubalance > 0, "No balance to withdraw!");
-
-        payable(owner).transfer(totalBalance);
-
-        // 清空 top3Depositors 和 top3Amounts
-        delete top3Depositors;
-        delete top3Amounts;
+        payable(owner).transfer(ubalance);
     }
 
-    // 3.获取存款金额前三名的信息
-    function getTopThree() external view returns (address[3] memory, uint256[3] memory) {
-        return (top3Depositors, top3Amounts);
+    function getTop3() external view returns (address[3] memory) {
+        return top3Depositors;
     }
 
-    // 更新存款金额前三名的信息
-    function updateTopThree(address account, uint256 amount) internal {
-        if (amount > top3Amounts[2]) {
-            if (amount > top3Amounts[0]) {
-                top3Amounts[2] = top3Amounts[1];
-                top3Amounts[1] = top3Amounts[0];
-                top3Amounts[0] = amount;
+    //收钱回调
+    receive() external payable { }
 
-                top3Depositors[2] = top3Depositors[1];
-                top3Depositors[1] = top3Depositors[0];
-                top3Depositors[0] = account;
-            } else if (amount > top3Amounts[1]) {
-                top3Amounts[2] = top3Amounts[1];
-                top3Amounts[1] = amount;
+    //没有function的方法会调用
+    fallback() external payable { }
 
-                top3Depositors[2] = top3Depositors[1];
-                top3Depositors[1] = account;
-            } else {
-                top3Amounts[2] = amount;
-                top3Depositors[2] = account;
-            }
-        }
-    }
-
-
-    // 查询用户存储
     function getBalance(address user) public view returns (uint) {
         return balances[user];
     }
 
     // 查询银行所有资产
     function getTotalBalance() public view returns (uint) {
-        return totalBalance;
+        return address(this).balance;
     }
 
 }
